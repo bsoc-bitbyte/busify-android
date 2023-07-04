38b23d37c7b965d6b22cb805d66b4e91f8219d76
Sadvika <sadvikachitteti@gmail.com>
2023-07-05 00:09:47 +0530

    made changes

--- app/src/main/java/com/example/busify/ui/QR/QRActivity.kt
+++ app/src/main/java/com/example/busify/ui/QR/QRActivity.kt
@@ -1,23 +1,38 @@
 package com.example.busify.ui.QR
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.busify.R
 import com.example.busify.databinding.ActivityQractivityBinding
 import com.example.busify.models.PassengerRVModel
 
 class QRActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityQractivityBinding
+    private lateinit var binding:ActivityQractivityBinding
+    private var qrRVData: List<PassengerRVModel> ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQractivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var passengerRVData = arrayListOf<PassengerRVModel>(
+        qrRVData = arrayListOf<PassengerRVModel>(
             PassengerRVModel("Passenger 1", true),
             PassengerRVModel("Passenger 2", false),
             PassengerRVModel("Passenger 3", true),
             PassengerRVModel("Passenger 4", false)
         )
+    setupRV()
     }
-}
+    private fun setupRV(){
+        val rvadapter = QrRVAdapter(qrRVData!!)
+        binding.apply {
+            verifiedListRv.apply {
+                setHasFixedSize(true)
+                layoutManager = LinearLayoutManager(this@QRActivity)
+                adapter = rvadapter
+            }
+        }
+    }
+
+    }
--- app/src/main/java/com/example/busify/ui/QR/QrRVAdapter.kt
+++ app/src/main/java/com/example/busify/ui/QR/QrRVAdapter.kt
@@ -1,4 +1,35 @@
 package com.example.busify.ui.QR
 
-class QrRVAdapter {
-}
+import android.icu.text.IDNA.Info
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.busify.databinding.QrItemsBinding
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.busify.models.PassengerRVModel
+
+class QrRVAdapter(private var qrItems: List<PassengerRVModel>):
+    RecyclerView.Adapter<QrRVAdapter.ViewHolder>() {
+    class ViewHolder(binding:QrItemsBinding) : RecyclerView.ViewHolder(binding.root) {
+        val username = binding.username
+        val info = binding.info
+        val verifyswitch=binding.verifySwitch
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(QrItemsBinding.inflate(
+            LayoutInflater.from(parent.context),parent,false
+        ))
+    }
+    override fun getItemCount(): Int {
+        return qrItems.size
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model = qrItems[position]
+        holder.username.text = model.name
+       holder.verifyswitch.isChecked=model.isVerified
+
+    }
+
+    }
+
